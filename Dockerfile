0c055729361a4f6d7b632eb46284f5dbd01c76ad
Matthew Baggett <matthew@baggett.me>
2017-07-31 18:14:09 +0100

    add flashing support maybe?

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM gone/python-arm
 COPY .docker/service /etc/service
 RUN chmod +x /etc/service/*/run
-RUN pip install blinkt
--- process.py
+++ process.py
@@ -9,11 +9,20 @@ blinkt.set_brightness(1)
 
 r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0)
 for x in range(blinkt.NUM_PIXELS):
-    r.hmset('blinkt_light_state:' + str(x), {'r': 0, 'g': 0, 'b': 0, 'bri': 1});
+    r.hmset('blinkt_light_state:' + str(x), {'r': 0, 'g': 0, 'b': 0, 'bri': 1, 'mode': 'none'});
+
+iter = 0;
 
 while True:
+    iter += 1
     for x in range(blinkt.NUM_PIXELS):
         dict = r.hgetall('blinkt_light_state:' + str(x));
-        blinkt.set_pixel(x, float(dict['r']), float(dict['g']), float(dict['b']), float(dict['bri']))
+        if dict['mode'] == "flashing":
+            if iter % 2 == 0:
+                blinkt.set_pixel(x, float(dict['r']), float(dict['g']), float(dict['b']), float(dict['bri']))
+            else:
+                blinkt.set_pixel(x, 0,0,0,0)
+        else:
+            blinkt.set_pixel(x, float(dict['r']), float(dict['g']), float(dict['b']), float(dict['bri']))
     blinkt.show()
     time.sleep(0.5)
