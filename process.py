658d627004169009ffd0daa6487de0119806d3b5
Matthew Baggett <matthew@fouroneone.us>
2017-07-31 16:58:03 +0100

    set per-led brightness

--- process.py
+++ process.py
@@ -9,13 +9,11 @@ blinkt.set_brightness(1)
 
 r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0)
 for x in range(blinkt.NUM_PIXELS):
-    r.hmset('blinkt_light_state:' + str(x), {'r': 0, 'g': 0, 'b': 0});
+    r.hmset('blinkt_light_state:' + str(x), {'r': 0, 'g': 0, 'b': 0, 'bri': 1});
 
 while True:
     for x in range(blinkt.NUM_PIXELS):
         dict = r.hgetall('blinkt_light_state:' + str(x));
-#       print "%d: Setting pixel %d to RGB (%d,%d,%d)" % (itercount, x, float(dict['r']), float(dict['g']), float(dict['b']))
-        blinkt.set_pixel(x, float(dict['r']), float(dict['g']), float(dict['b']))
+        blinkt.set_pixel(x, float(dict['r']), float(dict['g']), float(dict['b']), float(dict['bri']))
     blinkt.show()
-#   print ""
     time.sleep(0.5)
