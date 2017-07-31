5ee80c6416c30cd00c9c2e2e4bcf2086f325471e
Matthew Baggett <matthew@baggett.me>
2017-07-31 16:29:20 +0100

    Remove some whitespace.

--- process.py
+++ process.py
@@ -11,10 +11,7 @@ r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT
 for x in range(blinkt.NUM_PIXELS):
     r.hmset('blinkt_light_state:' + str(x), {'r': 0, 'g': 0, 'b': 0});
 
-itercount = 0;
-
 while True:
-    itercount += 1
     for x in range(blinkt.NUM_PIXELS):
         dict = r.hgetall('blinkt_light_state:' + str(x));
 #       print "%d: Setting pixel %d to RGB (%d,%d,%d)" % (itercount, x, float(dict['r']), float(dict['g']), float(dict['b']))
